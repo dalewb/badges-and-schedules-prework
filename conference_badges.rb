3b60b19de480672cffb22fc1979f371a6ef189db
William Dale <williambdale@gmail.com>
2018-03-05 18:14:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+speakers = %w(Edsger, Ada, Charles, Alan, Grace, Linus, Matz)
+
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(speakers)
+  speakers.map! {|el| badge_maker(el)}
+end 
+
