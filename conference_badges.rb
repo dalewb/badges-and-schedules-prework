c7a86eeccfcba9f77b80fb37e82df02f60e72bf4
William Dale <williambdale@gmail.com>
2018-03-05 18:14:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,22 @@
-speakers = %w(Edsger, Ada, Charles, Alan, Grace, Linus, Matz)
+speakers = %w(Edsger Ada Charles Alan Grace Linus Matz)
 
 def badge_maker(name)
   "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(speakers)
-  speakers.map! {|el| badge_maker(el)}
+  badges = speakers.map {|el| badge_maker(el)}
 end 
 
+def assign_rooms(speakers)
+  rooms = [] 
+  speakers.each_with_index do |name, idx|
+    rooms << "Hello, #{name}! You'll be assigned to room #{idx+1}"
+  end 
+  rooms 
+end 
+
+def printer(speakers)
+  batch_badge_creator(speakers)
+  assign_rooms(speakers)
+end 
